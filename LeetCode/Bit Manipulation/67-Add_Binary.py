3d32c24ae5b28a6e1fe6eb02c195522d8a98cb1a
Devendra (Kyouma) <hkyouma07@gmail.com>
2025-02-14 16:26:38 +0530

    some files have been added

--- LeetCode/Bit Manipulation/67-Add_Binary.py
+++ LeetCode/Bit Manipulation/67-Add_Binary.py
@@ -26,20 +26,23 @@ Each string does not contain leading zeros except for the zero itself.
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
-        res = ""
-        carry = 0
+        res = ""  #empty string to store the result
+        carry = 0  #initialize carry to 0
 
-        a,b = a[::-1] , b[::-1]
+        a,b = a[::-1] , b[::-1]  #reverse the strings
 
-        for i in range(max(len(a), len(b))):
-            digitA = int(a[i]) - int("0") if i < len(a) else 0
+        for i in range(max(len(a), len(b))): #iterate through the strings (KEEP IN MIND THAT THE STRINGS ARE REVERSED)
+
+            #convert the character to integer and make sure to handle the case where the index is out of range
+            digitA = int(a[i]) - int("0") if i < len(a) else 0 
             digitB = int(b[i]) - int("0") if i < len(b) else 0
 
+            #calculate the sum of the digits and the carry
             total = digitA + digitB + carry
-            char = str(total % 2)
-            res = char + res
+            char = str(total % 2)  #get the character to be added to the result
+            res = char + res    #add the character to the result
 
-            carry = total //2
+            carry = total //2 #update the carry
 
         if carry:
             res = "1" + res
